a06071105d16ce62665f5245db4445f0e35d7e54
Pedro Deixler <ppdeixler@hotmail.com>
2024-04-14 15:12:54 -0300

    Initial Commit

---
+++ Assets/Tiles/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private int cellMapX, cellMapY;
+
+    private GameObject[] cells;
+
+    private void Start()
+    {
+        
+    }
+
+    private void GetCells()
+    {
+        
+    }
+
+}
---
+++ Assets/Tiles/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerController : MonoBehaviour
+{
+
+    private Vector2 mousePosition;
+
+    [SerializeField]
+    private GameManager gameManager;
+    private void Update()
+    {
+    }
+
+
+
+
+    public void Click()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(mousePosition), Vector2.zero);
+
+        if (hit.collider == null) return;
+
+        Tile tile = hit.collider.GetComponent<Tile>();
+
+        tile.SelectThis();
+    }
+    public void GetMousePosition(InputAction.CallbackContext context)
+    {
+        mousePosition = context.ReadValue<Vector2>();
+    }
+    public void OnClick(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Click();
+        }
+    }
+}
