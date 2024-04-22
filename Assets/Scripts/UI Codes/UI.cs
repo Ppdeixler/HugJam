8c2443157b4da145a720f3535d0cbd5468c9087f
Pedro Deixler <ppdeixler@hotmail.com>
2024-04-22 15:11:06 -0300

    Merge branch 'duda'

--- Assets/Scripts/UI Codes/UI.cs
+++ Assets/Scripts/UI Codes/UI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 public class UI : MonoBehaviour
 {
     [SerializeField]
@@ -13,12 +14,59 @@ public class UI : MonoBehaviour
     [SerializeField]
     private TMP_Text hospitals;
 
+
+    //MENU PAUSE
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUi;
+    public string menuScene = "MainMenu";
+
     private void Update()
     {
         houses.text = Resources.houses.ToString();
         firefighters.text = Resources.firefightbuild.ToString();
         hospitals.text = Resources.hospital.ToString();
+
+        //MENU PAUSE
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+
+            }
+            else
+            {
+                Pause();
+            }
+        }
     }
 
+    //MENU PAUSE
+    public void Resume()
+    {
+        pauseMenuUi.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
 
+    void Pause()
+    {
+        pauseMenuUi.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+        FindObjectOfType<AudioManager>().Stop("Theme");
+        FindObjectOfType<AudioManager>().Play("MainMenuThemeLoop");
+        SceneManager.LoadScene(menuScene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
--- Assets/UI/MainMenu.cs
+++ Assets/UI/MainMenu.cs
@@ -2,14 +2,79 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public string gameScene = "Game";
+    public AudioMixer audioMixer;
+    public TMPro.TMP_Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && 
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    //Main Menu
     public void StartGame() {
-        SceneManager.LoadSceneAsync("SampleScene");
+        FindObjectOfType<AudioManager>().Stop("MainMenuThemeLoop");
+        FindObjectOfType<AudioManager>().Play("Theme");
+        SceneManager.LoadSceneAsync(gameScene);
     }
 
     public void QuitGame() {
         Application.Quit();
+    }
+
+
+    //Settings Menu
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("masterVolume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    //Click audio
+    public void onClickSfx()
+    {
+        FindObjectOfType<AudioManager>().Play("Click");
     }
 }
